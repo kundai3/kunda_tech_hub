a8ad434fa862a5b4205c610a1e77b690ab90303c
kundai3 <kundaingwereva3@gmail.com>
2026-07-03 20:11:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio website loaded!");
+
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", function () {
+        alert("Welcome to my portfolio!");
+    });
+});
